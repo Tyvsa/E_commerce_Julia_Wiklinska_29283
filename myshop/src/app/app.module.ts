0c6630e1deef1080e910417549763ec17ff65bf8
Your Name <you@example.com>
2021-03-10 09:54:41 +0100

    Dodano routing, landing i products

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProductsComponent } from './components/products/products.component';
+
+const appRoutes: Routes = [{ path: '', redirectTo: '/landing', pathMatch: 'full' },
+{ path: 'landing', component: LandingComponent },
+{ path: 'products', component: ProductsComponent },];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ProductsComponent
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
